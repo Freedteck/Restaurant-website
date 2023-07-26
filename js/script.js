55432009ad4f34509a2ee46d34e859a326124baf
Freedteck <freedteck@gmail.com>
2023-07-26 15:58:49 +0100

    delete ajaxUtils.js, create snippets/menu-snippet, and edit the script.js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,37 @@
-$(function () {
-    
-    $("#navbar-toggle").blur(function (event) {
-        let screenWidth = window.innerWidth;
-        if (screenWidth < 768) {
-            $("#navbarSupportedContent").collapse('hide');
-        }
-    });
-    (function (window) {
+let homeHtml = "snippets/home-snippet.html";
+let menuHtml = "snippets/menu-snippet.html";
 
-    let dc = {}
-    
-    let homeHtml = "snippets/home-snippet.html";
-    
-    let insertHtml = function (selector, html) {
-        let targetElement = document.querySelector(selector);
-        targetElement.innerHTML = html;
-    };
-    
-    let showLoading = function (selector) {
-        let html = '<div class="text-center">'
-        html += '<img src="images/load.gif"></div>'
-        insertHtml(selector, html)
-    };
-    
-    $(function () {
-    
-        showLoading("#main-content");
-        $ajaxUtils.sendGetRequest(homeHtml, "#main-content", false)
-    });
-    
-    window.$dc = dc;
-})(window);
+document.addEventListener("DOMContentLoaded", function () {
+  // Load the initial content on page load
+  senGetRequest("main-content", homeHtml);
 
+  // Event for "Menu" link inside "snippets/menu-snippet.html" This isn't working,
+  document.getElementById("main-content").addEventListener("click", function (event) {
+    if (event.target && event.target.id === "menuLink") {
+      //event.preventDefault(); // Prevent the default link behavior
+      senGetRequest("main-content", menuHtml);
+      console.log("OK"); // This is not logged
+    } else {
+      console.log("Bad request") // This is logged in the console instead
+    }
+  });
 });
 
+
+// Writing the Ajax Function
+function senGetRequest(selector, contentToLoad) {
+  let request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    if (request.status === 200 && request.readyState == 4) {
+      // Clear existing content before loading new content
+      document.getElementById(selector).innerHTML = '';
+
+      // Load the new content
+      document.getElementById(selector).innerHTML = this.responseText;
+    } else {
+      console.log("Error loading content from " + contentToLoad);
+    }
+  };
+  request.open("GET", contentToLoad, true);
+  request.send();
+}
